ea0ffaa8891becb5793f6f80fda77c2361fa9ddb
Elyes Farjallah <92794212+elyesfarjallah@users.noreply.github.com>
2023-12-31 14:54:56 +0100

    ADD shell script to execute batch job on server

---
+++ execute_optimization.sh
@@ -0,0 +1,6 @@
+cd IRD_DeepLearning
+conda activate ird_deep_learning
+python main.py --model_keys vit_h_14 vit_b_16 resnext50_32x4d --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --study_save_path studies --n_epochs_validation 1 --prefered_device cuda:0 &
+python main.py --model_keys vit_l_32 swin_v2_t resnet18 resnet34 resnet50 swin_v2_b --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --study_save_path studies --n_epochs_validation 1 --prefered_device cuda:1 &
+python main.py --model_keys vit_b_32 swin_v2_s resnet101 resnet152 --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --study_save_path studies --n_epochs_validation 1 --prefered_device cuda:2 &
+python main.py --model_keys vit_l_16 resnext50_32x4d resnext101_32x8d --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --study_save_path studies --n_epochs_validation 1 --prefered_device cuda:3 &
