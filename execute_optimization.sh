e0aac9729df94dae6b9e5ee062428118d71eaa43
Elyes Farjallah <92794212+elyesfarjallah@users.noreply.github.com>
2024-01-02 19:15:24 +0100

    FIX conda.sh path

--- execute_optimization.sh
+++ execute_optimization.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This is a Slurm batch script for executing optimization
 
-source ~/elyes/miniconda3/etc/profile.d/conda.sh
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ird_deep_learning
 python main.py --model_keys vit_h_14 vit_b_16 resnext50_32x4d --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --study_save_path studies --n_epochs_validation 1 --prefered_device cuda:0 &
 python main.py --model_keys vit_l_32 swin_v2_t resnet18 resnet34 resnet50 swin_v2_b --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --study_save_path studies --n_epochs_validation 1 --prefered_device cuda:1 &
