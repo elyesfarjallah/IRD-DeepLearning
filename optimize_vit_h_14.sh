4913601a8a27be71c9a790e5316d3b73f20f4c6a
Elyes Farjallah <92794212+elyesfarjallah@users.noreply.github.com>
2024-01-03 20:04:48 +0100

    REMOVE arg study_save_path

--- optimize_vit_h_14.sh
+++ optimize_vit_h_14.sh
@@ -4,4 +4,4 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ird_deep_learning
 cd ~/IRD-DeepLearning
-nohup python main.py --model_keys vit_h_14 --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --study_save_path studies --n_epochs_validation 1 --prefered_device cpu & > vit_h_14.out &
+nohup python main.py --model_keys vit_h_14 --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --n_epochs_validation 1 --prefered_device cuda:0 --batch_size_options 32 & > nohup2.out &
