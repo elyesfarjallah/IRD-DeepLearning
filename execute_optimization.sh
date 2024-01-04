40fd6d1cf4171e7949bf5b9fb1e26f0b20256e89
Elyes Farjallah <92794212+elyesfarjallah@users.noreply.github.com>
2024-01-04 16:47:59 +0100

    ADD echo after first python call

--- execute_optimization.sh
+++ execute_optimization.sh
@@ -7,6 +7,7 @@ cd ~/IRD-DeepLearning
 echo 'Start optimization'
 rm vit_h_14.out vit_l_32.out vit_b_32.out resnext.out
 nohup python main.py --model_keys vit_h_14 --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --n_epochs_validation 1 --prefered_device cuda:0 --batch_size_options 2 4 8 >vit_h_14.out &
+echo 'vit_h_14 done'
 nohup python main.py --model_keys vit_l_32 --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --n_epochs_validation 1 --prefered_device cuda:1 --batch_size_options 2 4 8 >vit_l_32.out &
 nohup python main.py --model_keys vit_b_32 --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --n_epochs_validation 1 --prefered_device cuda:2 --batch_size_options 2 4 8 >vit_b_32.out &
 nohup python main.py --model_keys resnext50_32x4d resnext101_32x8d --dataset_path datasets/2023-12-28_18-12-43 --n_epochs 30 --wandb_config_path wandb_config.json --n_trials 20 --n_epochs_validation 1 --prefered_device cuda:3 --batch_size_options 2 4 8 16 32 >resnext.out &
