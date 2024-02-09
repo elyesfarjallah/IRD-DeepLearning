666e46889b4d4be478fea3b401c7acfaf1a090cc
Elyes Farjallah <92794212+elyesfarjallah@users.noreply.github.com>
2024-02-09 08:49:09 +0100

    CHANGE

--- shell_scripts/execute_multilable_optimization.sh
+++ shell_scripts/execute_multilable_optimization.sh
@@ -6,6 +6,6 @@ conda activate ird_deep_learning
 cd ~/IRD-DeepLearning
 echo 'Start optimization'
 nohup python train.py --model_key resnet18 --lr_min 1e-5 --lr_max 1e-1 --batch_size_options 16 32 64 128 --device cuda:0 &
-nohup python train.py --model_key vit_b_32 --lr_min 1e-7 --lr_max 1e-4 --batch_size_options 8 16 32 --device cuda:1 &
+nohup python train.py --model_key vit_h_14 --lr_min 1e-7 --lr_max 1e-4 --batch_size_options 8 16 32 --device cuda:1 &
 wait
 echo 'Optimization done'
