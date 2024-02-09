cbfb6f5545a95ae682a5594e1d29745e48bfdd72
Elyes Farjallah <92794212+elyesfarjallah@users.noreply.github.com>
2024-02-09 01:24:54 +0100

    ADD shell script to execute multilabel optimization

---
+++ shell_scripts/execute_multilable_optimization.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This is a Slurm batch script for executing optimization
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate ird_deep_learning
+cd ~/IRD-DeepLearning
+echo 'Start optimization'
+nohup python train.py --model_key resnet18 --lr_min 1e-5 --lr_max 1e-1 --batch_size_options 16 32 64 128 --device cuda:0
+nohup python train.py --model_key vit_h_14 --lr_min 1e-7 --lr_max 1e-4 --batch_size_options 16 32 64 --device cuda:1
+wait
+echo 'Optimization done'
