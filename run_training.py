1dd8de4649f82c182fed0fe9660848633d8213ef
elyesfarjallah <farjallahelyes@yahoo.com>
2024-05-27 17:34:31 +0200

    ADD python script to run trainings

---
+++ run_training.py
@@ -0,0 +1,165 @@
+from data_pipeline.np_dataset import NpDataset
+import input_mapping.models_torch as models_torch
+from data_pipeline.image_transforms import get_transforms
+
+from data_pipeline.data_package import DataPackage
+
+from PIL import Image
+from pydicom import dcmread
+import torch
+from torch.utils.data import DataLoader, ConcatDataset
+from torchvision import transforms
+import numpy as np
+
+from ai_backend.loggers.model_logger import is_min
+from uuid import uuid4
+import torch.nn as nn
+from torch.optim import Adam
+import torch
+import json
+import os
+import re
+import tqdm
+from ai_backend.evaluators.metrics.multi_label_metrics import  multi_label_f_beta, multi_label_confusion_matrix, multi_label_accuracy, multi_label_precision, multi_label_recal
+import numpy as np
+from sklearn.metrics import ConfusionMatrixDisplay, multilabel_confusion_matrix
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+#checkpoint 1
+augmentations = transforms.Compose([
+                transforms.RandomRotation(10),
+                transforms.RandomHorizontalFlip(p=0.6),
+                transforms.RandomVerticalFlip(p=0.5)
+            ])
+#create np datasets for training, validation and testing
+read_dicom = lambda x: dcmread(x).pixel_array
+dicom_file_reader = lambda x: Image.fromarray(read_dicom(x)).convert('RGB')
+default_file_reader = lambda x: Image.open(x).convert('RGB')
+
+model_key = 'resnet18'
+transform_type = 'standard'
+batch_size = 128
+lr = 0.00009354747253832916
+epochs = 30
+transforms_config = models_torch.model_dict[model_key]['transforms_config']
+transform = get_transforms(transform_name = transform_type, transforms_config = transforms_config)
+labels_to_encode = np.array(["Age-related Macular Degeneration", "Best Disease", "Bietti crystalline dystrophy",
+                              "cataract", "Cone Dystrophie or Cone-rod Dystrophie", "Diabetic Retinopathy",
+                              "glaucoma", "Maculopathy", "Myopia", "Normal", "Retinitis Pigmentosa", "Stargardt Disease"])
+def convert_package_to_dataset(package, augmentations=None):
+    file_reader = dicom_file_reader if package.data_source_name == 'UKB' else default_file_reader
+    dataset = NpDataset(file_paths=package.get_data(), labels=package.get_labels(),
+                         file_reader=file_reader, transform=transform, augmentation_transform=augmentations)
+    return dataset
+
+def convert_package_list_to_dataset(package_list, augmentations=None):
+    datasets = []
+    for package in package_list:
+        dataset = convert_package_to_dataset(package)
+        datasets.append(dataset)
+    return datasets
+#list the saved directories and load the datapackages
+dataset_path = 'datasets/2024-05-17_12-13-57'
+train_packages_path = f'{dataset_path}/train'
+val_packages_path = f'{dataset_path}/val'
+test_packages_path = f'{dataset_path}/test'
+package_paths = [train_packages_path, val_packages_path, test_packages_path]
+train_packages = []
+val_packages = []
+test_packages = []
+all_packages = [train_packages, val_packages, test_packages]
+
+for path, package_list  in zip(package_paths, all_packages):
+    files = os.listdir(path)
+    for file in files:
+        package = DataPackage.load(f'{path}/{file}')
+        package_list.append(package)
+#convert to np datasets
+train_datasets = convert_package_list_to_dataset(train_packages, augmentations=augmentations)
+val_datasets = convert_package_list_to_dataset(val_packages)
+test_datasets = convert_package_list_to_dataset(test_packages)
+
+for i, dataset in  enumerate(train_datasets):
+    dataset.balance_augmentation()
+#concat the datasets
+train_dataset = ConcatDataset(train_datasets)
+validation_dataset = ConcatDataset(val_datasets)
+test_dataset = ConcatDataset(test_datasets)
+
+#create data loaders
+num_workers = 8
+train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=True)
+validation_loader = DataLoader(validation_dataset, batch_size=batch_size, num_workers=num_workers)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers)
+# create model
+model = models_torch.get_model(model_name=model_key, num_classes=len(labels_to_encode))
+
+criterion = nn.BCEWithLogitsLoss()
+optimizer = Adam(model.parameters(), lr=lr)
+#add dropout forward hooks to the model
+for name, module in model.named_modules():
+    re_pattern = re.compile(r'^layer\d+$')
+    if re_pattern.match(name) is not None:
+        print('Adding forward hook for:', name)
+        module.register_forward_hook(lambda module, input,
+                                      output: torch.nn.functional.dropout2d(output, p=0.2, training=module.training))
+model_id = str(uuid4())
+
+#train the model
+dataset_name = '2024-05-17_12-13-57'
+best_model_save_folder = f'models/{model_key}/{model_id}'
+best_model_save_path = f'{best_model_save_folder}/weights.pt'
+os.makedirs(best_model_save_folder, exist_ok=True)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#create model configuration
+model_config = {'model_key': model_key, 'transform_type': transform_type, 'batch_size': batch_size, 'lr': lr, 'epochs': epochs,
+                'labels_to_encode': labels_to_encode.tolist(), 'model_id': model_id, 'dataset_name': dataset_name}
+
+epochs = 30
+progress_bar = tqdm.tqdm(range(epochs))
+validation_loss_criterion = nn.BCEWithLogitsLoss()
+validation_losses = []
+best_validation_loss = np.inf
+#move the model to the device
+model.to(device)
+#only run training if no model has been saved yet
+if not os.path.exists(best_model_save_path):
+    for epoch in progress_bar:
+        model.train()
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+        model.eval()
+        with torch.no_grad():
+            validaton_outputs = []
+            validaton_labels = []
+            for inputs, labels in validation_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                validaton_outputs.append(outputs)
+                validaton_labels.append(labels)
+            #concatenate the outputs and labels
+            validaton_outputs = torch.cat(validaton_outputs, dim=0)
+            validaton_labels = torch.cat(validaton_labels, dim=0)
+            loss_validation = validation_loss_criterion(validaton_outputs, validaton_labels).item()
+            if is_min(loss_validation, best_validation_loss):
+                best_validation_loss = loss_validation
+                torch.save(model.state_dict(), best_model_save_path)
+                #update the model configuration
+                model_config['epoch'] = epoch
+                #best validation loss
+                model_config['best_validation_loss'] = best_validation_loss
+                #save the model configuration
+                with open(f'{best_model_save_folder}/model_config.json', 'w') as f:
+                    json.dump(model_config, f)
+            validation_losses.append(loss_validation)
+            #update the progress bar
+            progress_bar.set_postfix({'Loss validation': loss_validation, 'best validation loss': min(validation_losses)})
+        
